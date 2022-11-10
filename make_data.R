ad0859e07ff04b193c851fef01c41e6f0411648d
Dominique Makowski <dom.mak19@gmail.com>
2022-11-10 09:44:32 +0800

    rename

--- make_data.R
+++ make_data.R
@@ -1,23 +1,23 @@
-# Preprocessing 
+# Preprocessing
 preprocess_raw <- function(file) {
- 
+
   # Read data
   data<- read.csv(file)
-  
+
   if (!"final_results" %in% data$screen) {
     print(paste0("Warning: Incomplete data for ", file))
     return(data.frame())
   }
-  
+
   # Get Demographics and Browser Info
   dem <- data[data$screen=='demographics' & !is.na(data$screen), "response"]
   info <- data[data$screen == 'browser_info' & !is.na(data$screen),  ]
-  
+
   # Filter practice trials
   if ("practice_debrief" %in% data$screen) {
     data <- data[which(data$screen == "practice_debrief"):nrow(data), ]
   }
-  
+
   # Sort by random condition
   if ('1' %in% data$condition){
     # Illusion task data
@@ -26,14 +26,14 @@ preprocess_raw <- function(file) {
     }
     illusion_trials <- illusion_data[illusion_data$screen == "Trial", ]
     illusion_trials$Task <- 'Illusion'
-    
+
     # Perceptual task data
     if ("perceptual_instructions" %in% data$screen){
       perceptual_data <- data[which(data$screen =='perceptual_instructions'):nrow(data), ]
     }
     perceptual_trials <- perceptual_data[perceptual_data$screen =='Trial',]
     perceptual_trials$Task <- 'Perceptual'
-    
+
   } else {
     # Perceptual task data
     if ("break1" %in% data$screen){
@@ -41,7 +41,7 @@ preprocess_raw <- function(file) {
     }
     perceptual_trials <- perceptual_data[perceptual_data$screen == "Trial", ]
     perceptual_trials$Task <- 'Perceptual'
-    
+
     # Illusion task data
     if ("illusion_instructions" %in% data$screen){
       illusion_data <- data[which(data$screen =='illusion_instructions'):nrow(data), ]
@@ -49,13 +49,13 @@ preprocess_raw <- function(file) {
     illusion_trials <- illusion_data[illusion_data$screen =='Trial',]
     illusion_trials$Task <- 'Illusion'
   }
-  
-  
+
+
   # Get fixation cross position
   fixation<- data[data$screen=='fixation', 'stimulus']
   df <- gsub("<p style=\"color: black; font-size: 80px; ", '', (gsub("</p>", "", fixation)))
   fixation_position<- tidyr::separate(data.frame(position=df), col=position, into=c('Padding_Left', 'Padding_Right', 'Padding_Top', 'Padding_Bottom'), sep=';')
-  
+
   fixation_position$Padding_Bottom<-gsub("[[:punct:]]", "", fixation_position$Padding_Bottom)
   fixation_position$Padding_Bottom<-as.numeric(gsub('\\D+','', fixation_position$Padding_Bottom))
   fixation_position$Padding_Left<-as.numeric(gsub('\\D+','', fixation_position$Padding_Left))
@@ -63,11 +63,11 @@ preprocess_raw <- function(file) {
   fixation_position$Padding_Top<-as.numeric(gsub('\\D+','', fixation_position$Padding_Top))
   fixation_position$Fixation_Horizontal<- fixation_position$Padding_Left - fixation_position$Padding_Right
   fixation_position$Fixation_Vertical<- fixation_position$Padding_Top - fixation_position$Padding_Bottom
-  
+
   # Combine trial and fixation data
   trials <- rbind(illusion_trials, perceptual_trials)
   trials<- cbind(trials, fixation_position)
-  
+
   df <- data.frame(
     Participant = trials$participant_id,
     Age = as.numeric(jsonlite::fromJSON(dem[1])$age),
@@ -103,8 +103,8 @@ preprocess_raw <- function(file) {
     ISI = as.numeric(data[data$screen == "fixation" & !is.na(data$screen), "trial_duration"]),
     RT = as.numeric(trials$rt)
   )
-  
-  # 
+
+  #
   # if("IPIP6" %in% data$screen) {
   # IPIP6
   ipip6 <- as.data.frame(jsonlite::fromJSON(data[data$screen == "IPIP6", "response"]))
@@ -121,7 +121,7 @@ preprocess_raw <- function(file) {
                         sd(ipip6[grepl("Openness", names(ipip6))]),
                         sd(ipip6[grepl("HonestyHumility", names(ipip6))]),
                         sd(ipip6[grepl("Agreeableness", names(ipip6))])))
-  
+
   # PID5
   pid5 <- as.data.frame(jsonlite::fromJSON(data[data$screen == "PID5_SF", "response"]))
   df$PID5_Disinhibition <- rowMeans(pid5[grepl("Disinhibition", names(pid5))])
@@ -134,14 +134,14 @@ preprocess_raw <- function(file) {
                        sd(pid5[grepl("NegativeAffect", names(pid5))]),
                        sd(pid5[grepl("Antagonism", names(pid5))]),
                        sd(pid5[grepl("Psychoticism", names(pid5))])))
-  
+
   # PHQ-4
   phq4 <- as.data.frame(jsonlite::fromJSON(data[data$screen=='PHQ_4', 'response']))
   df$PHQ4_Anxiety<- rowMeans(phq4[grepl("Anxiety", names(phq4))])
   df$PHQ4_Depression<- rowMeans(phq4[grepl("Depression", names(phq4))])
   df$PHQ4_SD <- mean(c(sd(phq4[grepl("Anxiety", names(phq4))]),
                        sd(phq4[grepl("Depression", names(phq4))])))
-  
+
   # ASQ-28
   asq28<- as.data.frame(jsonlite::fromJSON(data[data$screen=='AQ28', 'response']))
   df$ASQ28_SocialSkills<- rowMeans(asq28[grepl("SocialSkills", names(asq28))])
@@ -154,9 +154,9 @@ preprocess_raw <- function(file) {
                        sd(asq28[grepl("Imagination", names(asq28))]),
                        sd(asq28[grepl("Switching", names(asq28))]),
                        sd(asq28[grepl("Patterns", names(asq28))])))
-  
+
   # GCBS (can also be measured as a unidimensional construct)
-  gcbs<- as.data.frame(jsonlite::fromJSON(data[data$screen=='GCBS', 'response']))  
+  gcbs<- as.data.frame(jsonlite::fromJSON(data[data$screen=='GCBS', 'response']))
   df$GCBS_GovernmentMalfeasance<-rowMeans(gcbs[grepl("GM", names(gcbs))])
   df$GCBS_ETCoverUp<-rowMeans(gcbs[grepl("ET", names(gcbs))])
   df$GCBS_MalevolentGlobalConspiracies<-rowMeans(gcbs[grepl("MG", names(gcbs))])
@@ -168,7 +168,7 @@ preprocess_raw <- function(file) {
                       sd(gcbs[grepl("MG", names(gcbs))]),
                       sd(gcbs[grepl("PW", names(gcbs))]),
                       sd(gcbs[grepl("CI", names(gcbs))])))
-  
+
   # SPQ
   spq<- as.data.frame(jsonlite::fromJSON(data[data$screen=='SPQ_BRU', 'response']))
   df$SPQ_MagicalThinking<- rowMeans(spq[grepl("MagicalThinking", names(spq))])
@@ -189,7 +189,7 @@ preprocess_raw <- function(file) {
                      sd(spq[grepl("NoCloseFriends", names(spq))]),
                      sd(spq[grepl("SocialAnxiety", names(spq))]),
                      sd(spq[grepl("Suspiciousness", names(spq))])))
-  
+
   # IAS
   ias<- as.data.frame(jsonlite::fromJSON(data[data$screen=='IAS_R', 'response']))
   df$IAS_Nociception<- rowMeans(ias[grepl("Nociception", names(ias))])
@@ -202,7 +202,7 @@ preprocess_raw <- function(file) {
                      sd(ias[grepl("Elimination", names(ias))]),
                      sd(ias[grepl("Interoception", names(ias))]),
                      sd(ias[grepl("Expulsion", names(ias))])))
-  
+
   # LIE
   lie<- as.data.frame(jsonlite::fromJSON(data[data$screen=='LIE', 'response']))
   df$LIE_Contextuality<- rowMeans(lie[grepl("Contextuality", names(lie))])
@@ -213,8 +213,8 @@ preprocess_raw <- function(file) {
                      sd(lie[grepl("Ability", names(lie))]),
                      sd(lie[grepl("Negativity", names(lie))]),
                      sd(lie[grepl("Frequency", names(lie))])))
-  
-  # Attention Check 
+
+  # Attention Check
   # IPIP6, IAS and LIE responses are made on a visual analog scale
   # Note jsPSych records Likert values from 0 (not 1)
   df$Attention_Check1<- ipip6$Attention_Check_1
@@ -224,8 +224,8 @@ preprocess_raw <- function(file) {
   df$Attention_Check5<- ias$Attention_Check_5
   df$Attention_Check6<- ifelse(gcbs$Attention_Check_6==0,1,0)
   df$Attention_Check7<- lie$Attention_Check_7
-    
-  #   
+
+  #
   # } else {
   #   df$IPIP6_Extraversion <- df$IPIP6_Conscientiousness <- df$IPIP6_Neuroticism <- df$IPIP6_Openness <- df$IPIP6_HonestyHumility <- df$IPIP6_Agreeableness <- df$IPIP6_SD <- NA
   #   df$PID5_Disinhibition <- df$PID5_Detachment <- df$PID5_NegativeAffect <- df$PID5_Antagonism <- df$PID5_Psychoticism <- df$PID5_SD <- NA
@@ -233,7 +233,11 @@ preprocess_raw <- function(file) {
   df
 }
 
-participants <- list.files("data/rawdata/")
+
+# Find participants
+rawdata_folder <- "C:/Dropbox/RECHERCHE/Data/IllusionGameReliability/"
+participants <- list.files(paste0(rawdata_folder, "session1/"))
+
 
 df <- data.frame()
 for (ppt in participants) {
@@ -242,5 +246,5 @@ for (ppt in participants) {
 
 
 write.csv(df, "data/data.csv", row.names = FALSE)
-  
-  
+
+
